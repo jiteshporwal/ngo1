64b0ade01e2a5875f40d0803ecbd3fcc06f79ebb
jitesh porwal <jiteshporwaljp18@gmail.com>
2025-12-11 00:25:44 +0530

    Fix: remove submodule reference and add frontend as a normal folder

---
+++ sevasamiti-platform/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Home Page</h1>
+      <p>This is your new React project. Start building here!</p>
+    </div>
+  );
+}
+export default App;
